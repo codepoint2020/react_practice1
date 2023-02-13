8e535089a52d4e102ea1a970515f82b28458361e
j-morales-code <jeromemorales86@live.com>
2023-02-13 20:26:05 +0800

    Practicing coding in react and git

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+    render() {
+        return(
+            <div className='tc f1'>
+                <h1>Welcome to my react website</h1>
+                <p>This is just a practice in coding with react.js</p>
+            </div>
+        );
+    }
+}
+
+
+export default Hello;
